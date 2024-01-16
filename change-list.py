4e32d4fe141a4991827127ff7bd058218eed5bb0
Eddie Ayala <heriberto.ayala1981@gmail.com>
2024-01-16 12:59:59 -0500

    sec commit

--- change-list.py
+++ change-list.py
@@ -2,8 +2,29 @@ numbers = [1, 2, 3, 4, 5, 99, 2600, 300]
 sorted(numbers)
 print(numbers)
 
-char = ["d", "e", "a", "l", "g", "t"]
-emptyChar = []
+word = "Hello"
+emptyList = []
 
-for cha in char:
-    emptyChar[cha]
+for letter in word:
+    emptyList.append(letter)
+
+emptyList.reverse()
+print(emptyList)
+
+emptyList2 = ""
+
+for letter in emptyList:
+    emptyList2 += letter
+
+print(emptyList2)
+
+
+names = ["Luffy", "Zoro", "Sanji", "Usopp", "Nami"]
+
+if "Luffy" in names:
+    names.remove("Luffy")
+else:
+    names.append("Luffy")   
+
+
+print(names)
